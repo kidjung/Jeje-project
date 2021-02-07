c73810360365e54b813f43fc6b3a2659b41c1b6e
kimsungho <kimsungho97@gmail.com>
2021-02-08 03:31:11 +0900

    견종 검색기능 구현 - 검색시 request param으로 한글 깨짐현상 발생함

--- src/main/java/Jeje_project/Jeje_project/controller/Admin_Controller.java
+++ src/main/java/Jeje_project/Jeje_project/controller/Admin_Controller.java
@@ -7,6 +7,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import Jeje_project.Jeje_project.service.Dog_Service;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import javax.servlet.http.HttpServletResponse;
 import java.io.PrintWriter;
@@ -35,10 +36,17 @@ public class Admin_Controller {
 
     //견종 추가, 삭제, 수정 페이지
     @GetMapping("/admin_page/dog_list")
-    public String dog_list(Model model){
-        List<Dog> dogs=dog_service.all_Dogs();
-        model.addAttribute("dogs", dogs);
-        return "admin_pages/dog_list";
+    public String dog_list(@RequestParam(value="name", required = false) String name, Model model){
+
+        try{
+            List<Dog> dogs = dog_service.search_Dogs(name.strip());
+            model.addAttribute("dogs", dogs);
+            return "admin_pages/dog_list";
+        } catch (NullPointerException e){
+            List<Dog> dogs = dog_service.all_Dogs();
+            model.addAttribute("dogs", dogs);
+            return "admin_pages/dog_list";
+        }
     }
 
 
@@ -80,4 +88,18 @@ public class Admin_Controller {
 
     }
 
+
+    //견종 검색 Post
+    //issue : request param으로 한글 깨짐현상 발생
+    @PostMapping(value="/search_dog",produces="text/plain;charset=UTF-8")
+    public String search_dog(Dog_Form dog_form, HttpServletResponse response) throws Exception{
+        response.setContentType("text/html;charset=UTF-8");
+        response.setCharacterEncoding("UTF-8");
+        if(dog_form.getName().strip().length()==0){
+            return "redirect:/admin_page/dog_list";
+        }
+        else {
+            return "redirect:/admin_page/dog_list?" + "name=" + dog_form.getName();
+        }
+    }
 }
